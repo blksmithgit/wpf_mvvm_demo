30cea79a396fca108de2d95c45e0e81cd8164ff8
blksmithgit <blksmithgit@sina.com>
2018-04-08 21:14:26 +0800

    实现静态绑定

---
+++ WpfDemo/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo
+{
+
+    
+
+    public class MainViewModel : ObservableObject
+    {
+        public Device device = new Device();
+        public string Name {
+                get { return device.Name; }
+            set { device.Name = value; }
+        }
+        public int Status {
+            get
+            {
+                return device.Status;
+            }
+            set
+            {
+                device.Status = value;
+            }
+        }
+
+        
+        
+        
+    }
+}
